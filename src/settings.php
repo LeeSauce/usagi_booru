09dd3ad1fe191c298817b07956ce73c7d4aaf5e6
gabelee <ligabe097@gmail.com>
2024-10-02 18:21:57 -0400

    Users can change their user and email while logged in

--- src/settings.php
+++ src/settings.php
@@ -1,3 +1,15 @@
+<?php
+    session_start();
+    require("class/User.inc.php");
+    require("class/DbConnect.inc.php");
+
+    $user = null;
+    if(!isset($_SESSION["USER"])){
+        header("Location: login.php");
+    }else{
+        $user = unserialize($_SESSION["USER"]);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +25,52 @@
 
 <body>
 <h1>&#x2764Usagi Booru&#x2764</h1>
+
+<fieldset>
+    <h2>Change user info:</h2>
+    <?php
+
+    ?>
+    <form method="post" action="settings.php">
+        <?php
+            $username = null;
+            $email = null;
+
+            if($user instanceof User){
+                $username = $user->getUsername();
+                $email = $user->getEmail();
+            }
+        echo("<input type='text' name='username' value='" .$username."' required aria-required='true'>");
+            echo("<input type='text' name='email' value='" .$email."' required aria-required='true'>");
+        ?>
+        <input type="submit" name="submit" value="Change" required aria-required="true">
+        <br>
+        <button><a href="change_password.php">Change Password</a></button>
+        <button><a href="index.php">Home</a></button>
+    </form>
+</fieldset>
+<?php
+    if(isset($_POST["submit"])){
+        $conn = new DbConnect("root", "xXDaTUiQQ123!?@", "USAGI_DB");
+
+        if(isset($_POST["username"]) && isset($_POST["email"]) && $user instanceof User){
+            $username = $_POST["username"];
+            $email = $_POST["email"];
+            $id = $user->getId();
+            $SQL = "UPDATE USER SET Username = '" .$username. "', Email = '" .$email. "' WHERE UserID = ?";
+
+            $db = $conn->connect();
+            $preparedStatement = $db->prepare($SQL);
+            $preparedStatement -> bind_param("i", $id);
+            $preparedStatement -> execute();
+            $db->close();
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $_SESSION["USER"] = serialize($user);
+            header("Location: index.php");
+        }
+    }
+?>
 
 </body>
 
